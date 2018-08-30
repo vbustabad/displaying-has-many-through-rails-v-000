892419f50cb5db35aff0aa449fe6d2366096e114
Veronica Bustabad <vbustabad@gmail.com>
2018-08-30 18:25:42 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,7 @@
 class PostsController < ApplicationController
 
-  def new
-    @post = Post.new
-<<<<<<< HEAD
-  end
-=======
-  end 
->>>>>>> 33bef716c46039c14086ba17bd4b3f3232eaa000
-
   def show
     @post = Post.find(params[:id])
   end
+  
 end
